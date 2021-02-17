d09429113478daf8c427dd6eea2a4c8225af0ae1
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-02-17 23:18:30 +0530

    Make all posts visible along with author names on the home page . For this use Populate method after using find to get all the posts , in order  to populate the users field in post and making it easy to retrieve author info

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,29 @@
 
+const db = require('../config/index');//mongoose
+const User = require('../models/user');
+const Post = require('../models/post');
+
+
+
 module.exports.home = function(req ,res){
 
+
+    Post.find({}).populate('user').exec(function(err,posts){
+
+        if(err){
+            console.log("error showing posts !");
+            return;
+        }
+
+        return res.render('home' , {
+            title:"home",
+            heading:"Welcome To Codial !",
+            PostList:posts
+        })
+    });
+ 
     // console.log(req.cookies);
     // res.cookie('user_id' , 100);
-    return res.render('home' , {
-        title:"home",
-        heading:"Welcome To Codial !"
-    })
-
 }
 
 module.exports.homeLogin = function(req ,res){
