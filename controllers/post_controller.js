b1bbda2c538b0c11efccd05c318dd4a75ea28693
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-02-20 03:56:12 +0530

    FIX : Accedently wrote fucntion to delete post in Comments Conntroller and made the route in the Comments controller

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -19,3 +19,38 @@ module.exports.createPost = function(req , res){
         return res.redirect('/');
     });
 }
+
+module.exports.destroy = function(req,res){
+    
+    //first find the post exits or not depending on the url
+    Post.findById(req.params.id , function(err , post){
+        //this param will contai the id of the user who made the post
+        
+        //this is should match with the id of the logged in user 
+        //user.id just covers our user._id into aa string automatically
+
+        if(err){
+            console.log("Error finding post to delete");
+            return;
+        }
+
+        //now we check if the person deleting the post must be authour of the post 
+        //we havent populated user yet , so it must be the user id
+        if(post.user == user.id){
+
+            //deleted the post 
+            post.Remove();
+            //now go to the db and dlete aall the comments from that post 
+            //the id of the post is still there in the param
+            Comments.deleteMany({post:req.params.id} , function(err){ //this will delete the comments whose post field has the id of our post
+                console.log(err)
+                    return;
+             });
+
+             return res.redirect('back');
+
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
--- routes/posts.js
+++ routes/posts.js
@@ -7,5 +7,5 @@ const passport = require('passport');
 const postController = require("../controllers/post_controller");
 
 router.post('/create' , passport.checkAuthentication , postController.createPost);
-
+router.get('/destroy' , passport.checkAuthentication , postController.destroy);
 module.exports = router;
