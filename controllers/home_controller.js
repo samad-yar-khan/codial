a5bda89f1dc9632243e7f82b3211a606237a0a54
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-02-22 19:43:29 +0530

    Make the code better in home controller by replacing the call back functions in the home controllers action function by Async-Await , This we help us avoid CallBackHell

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,32 +4,70 @@ const User = require('../models/user');
 const Post = require('../models/post');
 const Comments = require('../models/comment');
 
+//Previos Code which depended on call backs and would have lead to call back hell
+
+// module.exports.home = function(req ,res){
 
 
-module.exports.home = function(req ,res){
+//     Post.find({})
+//     .populate('user')
+//     .populate({ //prepolate comments aswelas the name user of each user bby nestiing
+//         path : 'comments',
+//         populate : {
+//             path: 'user'
+//         }
+//     })
+//     .exec(function(err,posts){
 
+//         if(err){
+//             console.log("error showing posts !");
+//             return;
+//         }
 
-    Post.find({})
-    .populate('user')
-    .populate({ //prepolate comments aswelas the name user of each user bby nestiing
-        path : 'comments',
-        populate : {
-            path: 'user'
-        }
-    })
-    .exec(function(err,posts){
-
-        if(err){
-            console.log("error showing posts !");
-            return;
-        }
-
-        User.find({} , function(err , users){
+//         User.find({} , function(err , users){
              
-            if(err){
-                return;
-            }
+//             if(err){
+//                 return;
+//             }
 
+
+//             return res.render('home' , {
+//                 title:"home",
+//                 heading:"Welcome To Codial !",
+//                 PostList:posts,
+//                 all_users : users
+//             });
+//         });
+
+      
+//     });
+ 
+//     // console.log(req.cookies);
+//     // res.cookie('user_id' , 100);
+// }
+
+
+//Better way of writing code using async await method which is similiar to promises
+//here we wait for an async req to process before going further
+
+module.exports.home = async function(req ,res){
+
+    try {
+            //first asyc req to find and populate the post aswell as the  nested comments
+            let posts = await Post.find({})
+            .populate('user')
+            .populate({ //prepolate comments aswelas the name user of each user bby nestiing
+                path : 'comments',
+                populate : {
+                    path: 'user'
+                }
+            });
+            //now our server wiill wiat for the above req to process and proceed fuerther if there are no error 
+            //if there are errors then they will be sent to catch
+
+            let users = await  User.find({});
+             //now our server wiill wiat for the above req to process and proceed fuerther if there are no error 
+            //if there are errors then they will be sent to catch
 
             return res.render('home' , {
                 title:"home",
@@ -37,14 +75,16 @@ module.exports.home = function(req ,res){
                 PostList:posts,
                 all_users : users
             });
-        });
 
-      
-    });
- 
-    // console.log(req.cookies);
+    } catch (error) {
+        console.log("ERROR : " , error);
+        return;
+    }
+    
+     // console.log(req.cookies);
     // res.cookie('user_id' , 100);
 }
+
 
 module.exports.homeLogin = function(req ,res){
     res.end('<h1>Login</h1>');
