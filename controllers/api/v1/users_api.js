35472313949658a166e9d9cc4aa301e910c2fc90
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-03-04 02:23:52 +0530

    create a users_api and make an action which lets a user sign in and ccreate a session and get a jwt in response to enable authorizaton tasks

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,33 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+
+module.exports.createSession = async function(req , res){
+
+    try {
+        let user = await User.findOne({email:req.body.email});
+        console.log(req.body.password);
+        if(!user || user.password != req.body.password){
+        
+            return res.json(422, {
+                message:"Invalid Username Or Password"           
+            });
+
+        }
+
+        return res.json(200,{
+            message :"Sign in sucessfull , here is your token . Keep it safe !",
+            data :{
+                token : jwt.sign(user.toJSON() , "codial" ,  {expiresIn : '100000'})//toJSON converts the user to json and codial is the secret encryption key usin which we decrypt and encrypt the jwt
+            }
+        })
+
+
+    } catch (error) {
+        return res.json(500 , {
+            message:"Internal Server Error!"
+        })
+    }
+
+
+}
+
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/posts' , require('./posts') )
+router.use('/posts' , require('./posts') );
+router.use('/users' , require('./user'));
 
 module.exports = router;
