061cba5c9c936734f8c15219f321ec1313542abf
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-03-09 04:03:23 +0530

    Setup the ejs template for the mail and link it to the comments_mailer so that all emails aur sent through this ejs template

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,13 +2,14 @@ const nodeMailer = require('../config/nodemailer');
 
 exports.newComment = (comment)=>{
 
-    console.log('inside new comment mailer')
+    console.log('inside new comment mailer');
+    let htmlSring = nodeMailer.renderTemplate({comment:comment} , '/comments/new_comment.ejs')
 
     nodeMailer.transporter.sendMail({
         from:"samad.ic19@nsut.ac.in",
         to : comment.user.email,
         subject: "New Comment Published!",
-        html :"<h1>Your Comment has been Published!</h1>"
+        html :htmlSring
     } , function(err,info){//write callback to check fr errors
         if(err){
             console.log("error in sending mail : " ,err);
