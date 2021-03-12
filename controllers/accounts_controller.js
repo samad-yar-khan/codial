8292e8e35208aabb0e91890021f183a5b57c1423
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-03-13 00:48:55 +0530

    Create action which creates tokens to reset password

--- controllers/accounts_controller.js
+++ controllers/accounts_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const crypto = require('crypto'); //pre isntalled in node now 
+const ResetPassToken = require('../models/reset_pass_token');
 
 module.exports.forgotPass = function(req , res){
 
@@ -11,3 +13,44 @@ module.exports.forgotPass = function(req , res){
 
 }
 
+module.exports.verifyEmail = function(req , res){
+    
+
+    //first we find the the email mentioned in the form
+    const userEmail = req.body.email;
+    console.log(req.body);
+
+    User.findOne({email : userEmail} , function(err , user){
+        if(err){
+            console.log("Error finding user in databsse for resetting of password : " ,err);
+            return;
+        }
+
+        if(user){
+            const accessToken =  crypto.randomBytes(20).toString('hex');//access token for email 
+
+            ResetPassToken.create({
+                user : user._id,
+                accessToken : accessToken ,
+                isValid : true 
+            } , function(err , token){
+
+                if(err){
+                    console.log("Error Creating Access Token " , err );
+                    return;
+                }
+
+                let resetUrl = `localhost:8000/accounts/reset/?accessToken=${accessToken}`;
+                console.log(resetUrl);
+                return res.redirect('back');
+            })
+
+        }else{
+            req.flash('error' , 'User Not Found !');
+            return res.redirect('back');
+        }
+    })
+    
+
+
+}
--- routes/accounts.js
+++ routes/accounts.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const accountsController = require('../controllers/accounts_controller');
 
 router.get('/forgot-pass' , accountsController.forgotPass);
-
+router.post('/verify-email' , accountsController.verifyEmail);
 module.exports = router;
