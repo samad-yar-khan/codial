73581f776c9c84601f177028f140a0a51f10764f
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-03-27 18:04:46 +0530

    Enable the user to join a room in fe , and join room , send a confirmation to fr , and recive the confirmation

--- assets/javascript/chat_engine.js
+++ assets/javascript/chat_engine.js
@@ -12,9 +12,26 @@ class chatEngine{
     }
 
     connectionHandler(){
-        //this will bascially fire an even to establish our conection
+
+        let self = this;
+
+        //1this will bascially fire an even to establish our conection
         this.socket.on('connect' , function(){
             console.log("connection using sockets !");
+            
+            //1)what we do is that we emit an event('join_room') from the front end which will tell the backend that a user wants to join a vhat room
+            //along with the emit we send some data 
+            //this req will be recived at the baackdend chat_socket
+            self.socket.emit('join_room' , {
+                user_email : self.userEmail,
+                chat_room : 'codial'
+            });
+
+            //4)we confiem if a user has joned the room
+            self.socket.on('user_joined' , function(data){
+                console.log("user joined " , data);
+            })
+
         });
 
     }
--- config/chat_socket.js
+++ config/chat_socket.js
@@ -1,6 +1,6 @@
 module.exports.chatSockets = function(socketServer){
 
-    let io= require('socket.io')(socketServer , {
+  let io= require('socket.io')(socketServer , {
         cors: {
           origin: "http://localhost:8000",
           methods: ["GET", "POST"],
@@ -13,6 +13,19 @@ module.exports.chatSockets = function(socketServer){
 
         socket.on('disconnect' , function(){
             console.log("Socket Disconnected !" );
+        })
+        //2)we detected the emited join_room event
+        socket.on('join_room' , function(data){
+
+          console.log( 'joining the requested room',data);
+          //we join the chatroom if it exists else its created 
+          socket.join(data.chat_room);
+          
+          //3)we must send a confirmation back to the front end broeserr taht our user has been added ,
+          //for that we emit an event confiming that user has joined
+          //we must emit this message only to the user  of that speceific room
+          io.in(data.chat_room ).emit('user_joined' , data);
+          //this confirmation is recved at the front end 
         })
     })
 
