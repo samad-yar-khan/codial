b7f9f33073aba816f13f5c753d8ea185b2b6f1a3
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-04-01 17:28:35 +0530

    Create an enviroment file for setting up the production and development environments

---
+++ config/environment.js
@@ -0,0 +1,16 @@
+
+const development = {
+    
+    name : 'development' 
+
+}
+
+
+const production = {
+
+    name : 'production'
+
+}  
+
+//we choose what needs to be exported 
+module.exports = development
