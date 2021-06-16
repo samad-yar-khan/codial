fe83707dc0cd85ed418582cbad7f278b1a669cbc
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-06-17 03:05:09 +0530

    Update users Api

--- controllers/api/v2/users_api.js
+++ controllers/api/v2/users_api.js
@@ -10,12 +10,14 @@ module.exports.createSession = async function(req , res){
         if(!user || user.password != req.body.password){
         
             return res.json(422, {
+                success:false ,
                 message:"Invalid Username Or Password"           
             });
 
         }
 
         return res.json(200,{
+            success:true,
             message :"Sign in sucessfull , here is your token . Keep it safe !",
             data :{
                 token : jwt.sign(user.toJSON() , environment.jwt_secret ,  {expiresIn : '1000000'})//toJSON converts the user to json and codial is the secret encryption key usin which we decrypt and encrypt the jwt
@@ -25,6 +27,7 @@ module.exports.createSession = async function(req , res){
 
     } catch (error) {
         return res.json(500 , {
+            success:false,
             message:"Internal Server Error!"
         })
     }
